82572fd4e2228fb3434f5121d91a62966271606d
Matthew Solarski <mxtthxw@gmail.com>
2016-04-22 16:41:13 -0400

    Added JS border functionality to gallery

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,6 @@
+var i = 1;
 
 $(document).ready(function(){
-
-		// $('#modal').on('click', function(){
-		// 	$('#modal').css("opacity", "1");
-		// })
 
 	$("#modal").click(function(){
 		$("#modal").animate({
@@ -11,9 +8,124 @@ $(document).ready(function(){
     	}, 800);
 	});
 
-	$("img").mouseover(function(){
-		// $("img").toggle.css("width", "210");
-		$("img").css("border", "10px solid white");
-	});
+	$("#img1").mouseenter(
+		function(){
+			$("#img1").css("border", "3px solid white");
+		}
+	);
+
+	$("#img1").mouseleave(
+		function(){
+			$("#img1").css("border", "none");
+		}
+	);
+
+	$("#img2").mouseenter(
+		function(){
+			$("#img2").css("border", "3px solid white");
+		}
+	);
+
+	$("#img2").mouseleave(
+		function(){
+			$("#img2").css("border", "none");
+		}
+	);
+
+	$("#img3").mouseenter(
+		function(){
+			$("#img3").css("border", "3px solid white");
+		}
+	);
+
+	$("#img3").mouseleave(
+		function(){
+			$("#img3").css("border", "none");
+		}
+	);
+
+	$("#img4").mouseenter(
+		function(){
+			$("#img4").css("border", "3px solid white");
+		}
+	);
+
+	$("#img4").mouseleave(
+		function(){
+			$("#img4").css("border", "none");
+		}
+	);
+
+	$("#img5").mouseenter(
+		function(){
+			$("#img5").css("border", "3px solid white");
+		}
+	);
+
+	$("#img5").mouseleave(
+		function(){
+			$("#img5").css("border", "none");
+		}
+	);
+
+	$("#img6").mouseenter(
+		function(){
+			$("#img6").css("border", "3px solid white");
+		}
+	);
+
+	$("#img6").mouseleave(
+		function(){
+			$("#img6").css("border", "none");
+		}
+	);
+
+	$("#img7").mouseenter(
+		function(){
+			$("#img7").css("border", "3px solid white");
+		}
+	);
+
+	$("#img7").mouseleave(
+		function(){
+			$("#img7").css("border", "none");
+		}
+	);
+
+	$("#img8").mouseenter(
+		function(){
+			$("#img8").css("border", "3px solid white");
+		}
+	);
+
+	$("#img8").mouseleave(
+		function(){
+			$("#img8").css("border", "none");
+		}
+	);
+
+	$("#img9").mouseenter(
+		function(){
+			$("#img9").css("border", "3px solid white");
+		}
+	);
+
+	$("#img9").mouseleave(
+		function(){
+			$("#img9").css("border", "none");
+		}
+	);
+
+	$("#img10").mouseenter(
+		function(){
+			$("#img10").css("border", "3px solid white");
+		}
+	);
+
+	$("#img10").mouseleave(
+		function(){
+			$("#img10").css("border", "none");
+		}
+	);
 	
 })
